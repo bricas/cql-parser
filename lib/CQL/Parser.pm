531eb10f2f717606675012ebf72d6aade7d9bf27
Wilbert Hengst <W.Hengst@uva.nl>
2006-01-27 15:00:41 +0000

    Incremented version to 0.97 and added handling new relation modifiers

--- lib/CQL/Parser.pm
+++ lib/CQL/Parser.pm
@@ -13,7 +13,7 @@ use CQL::PrefixNode;
 use CQL::ProxNode;
 use Carp qw( croak );
 
-our $VERSION = '0.96';
+our $VERSION = '0.97';
 
 my $lexer;
 my $token;
@@ -182,15 +182,8 @@ sub parseTerm {
 
         while ($token->getType() == CQL_MODIFIER ) {
             match( $token );
-            my $type = $token->getType();
-            if ( $type != CQL_RELEVANT and $type != CQL_FUZZY 
-                and $type != CQL_STEM and $type != CQL_PHONETIC
-                and $type != CQL_WORD ) {
-                croak( "expected relation modifier got ".$token->getString() );
-            }
-            if ( $type == CQL_WORD and $token->getString() !~ /\./ ) {
-                croak( "unknown first-class relation modifier: ".
-                    $token->getString() );
+            if ( !isRelationModifier() ) {
+                croak( "expected relation modifier got " . $token->getString() );
             }
             $relation->addModifier( $token->getString() );
             match( $token );
@@ -305,6 +298,17 @@ sub isProxRelation {
         or $type==CQL_GE or $type==CQL_NE );
 }
 
+sub isRelationModifier {
+    my $type = $token->getType();
+    if ($type == CQL_WORD) {
+        return $token->getString() =~ /\./;
+    }
+    return ($type==CQL_RELEVANT or $type==CQL_FUZZY or $type==CQL_STEM
+        or $type==CQL_PHONETIC or $type==CQL_PWORD or $type==CQL_STRING
+        or $type==CQL_ISODATE or $type==CQL_NUMBER or $type==CQL_URI
+        or $type==CQL_MASKED or $type==CQL_UNMASKED);
+}
+
 sub match {
     my $expected = shift;
     debug( "in match(".$expected->getString().")" );
@@ -383,7 +387,7 @@ for more information about Ockham.
 
 =item * Ed Summers - ehs at pobox dot com
 
-=item * Brian Cassidy - bricas at cpan dot org
+=item * Brian Cassidy - brian.cassidy at gmail dot com
 
 =item * Wilbert Hengst - W.Hengst at uva dot nl
 
